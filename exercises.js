bc1530b13b5a38571c26edba7dbc592001a5fe49
joanb123 <85811614+joanb123@users.noreply.github.com>
2024-02-05 21:41:07 -0500

    created exercises

---
+++ exercises.js
@@ -0,0 +1,36 @@
+// 3-1. flattening(list)
+let arrays = [[1, 2, 3], [4, 5], [6]];
+// Flattening the array
+let flattened = arrays.reduce((acc, currentArray) => acc.concat(currentArray), []);
+console.log(flattened);
+// → [1, 2, 3, 4, 5, 6]
+
+
+
+// 3-2. loop(value, test, update, body)
+function loop(start, test, update, body) {
+    for (let value = start; test(value); value = update(value)) {
+      body(value);}}
+  loop(3, n => n > 0, n => n - 1, console.log);
+  // → 3
+  // → 2
+  // → 1
+  
+
+// 3-3. everyLoop(array, test)
+function every(array, test) {
+    for (let element of array) {
+      if (!test(element)) {
+        return false; } }
+    return true; }
+
+// 3-4. everySome(array, test)
+function every(array, test) {
+    return !array.some(element => !test(element)); }
+
+    console.log(everyLoop([1, 3, 5], n => n < 10));
+    // → true
+    console.log(everyLoop([2, 4, 16], n => n < 10));
+    // → false
+    console.log(everyLoop([], n => n < 10));
+    // → true
